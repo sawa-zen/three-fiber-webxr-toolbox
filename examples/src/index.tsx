4a34b919ba8e3058613f8b40d0fa9425f02f5ab5
SAWADA Takayoshi <sawadasuiren@gmail.com>
2024-03-07 02:57:59 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! feat: Add remote display

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -12,15 +12,15 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     />
     <Canvas>
       <XR>
-        <RemoteDisplay
-          serverUrl={import.meta.env.VITE_SERVER_URL as string}
-          position={[0, 1.5, -1]} 
-        /> 
-        <XrErrorBoundary>
-          <ConsoleProvider>
-            <App />
-          </ConsoleProvider>
-        </XrErrorBoundary>
+        <ConsoleProvider>
+          <RemoteDisplay
+            serverUrl={import.meta.env.VITE_SERVER_URL as string}
+            position={[0, 1.1, -1.1]}
+          /> 
+          <XrErrorBoundary>
+              <App />
+          </XrErrorBoundary>
+        </ConsoleProvider>
       </XR>
     </Canvas>
   </React.StrictMode>
--- src/components/RemoteDisplay/index.tsx
+++ src/components/RemoteDisplay/index.tsx
@@ -1,42 +1,46 @@
-import { BackSide, DisplayP3ColorSpace } from "three"
+import { BackSide, DisplayP3ColorSpace, Group } from "three"
 import { useRemoteDisplay } from "./hooks"
 import { Interactive } from "@react-three/xr"
 import { memo } from "react"
 
 interface Props {
   position?: [number, number, number]
+  rotation?: [number, number, number]
   serverUrl: string
 }
 
 export const RemoteDisplay = memo(({
   position = [0, 0, 0],
+  rotation = [0, 0, 0],
   serverUrl,
 }: Props) => {
   const { videoElement, handleOnSelect } = useRemoteDisplay({ serverUrl })
 
   return (
-    <Interactive onSelect={handleOnSelect}>
-      <mesh position={position}>
-        <cylinderGeometry 
-          args={[
-            3, // radiusTop?: number,
-            3, // radiusBottom?: number,
-            2.5, // height?: number,
-            24, // radialSegments?: number,
-            5, // heightSegments?: number,
-            true, // openEnded?: boolean,
-            Math.PI + (Math.PI / 4.4), // thetaStart?: number,
-            -(Math.PI / 2.2), // thetaLength?: number,
-          ]}
-        />
-        <meshBasicMaterial>
-          <videoTexture
-            attach="map"
-            args={[videoElement]}
-            colorSpace={DisplayP3ColorSpace}
+    <group position={position} rotation={rotation} scale={[0.3, 0.3, 0.2]}>
+      <Interactive onSelect={handleOnSelect}>
+        <mesh position={[0, 0, 1.5]}>
+          <cylinderGeometry 
+            args={[
+              3, // radiusTop?: number,
+              3, // radiusBottom?: number,
+              2.5, // height?: number,
+              24, // radialSegments?: number,
+              5, // heightSegments?: number,
+              true, // openEnded?: boolean,
+              Math.PI + (Math.PI / 4.4), // thetaStart?: number,
+              -(Math.PI / 2.2), // thetaLength?: number,
+            ]}
           />
-        </meshBasicMaterial>
-      </mesh>
-    </Interactive>
+          <meshBasicMaterial>
+            <videoTexture
+              attach="map"
+              args={[videoElement]}
+              colorSpace={DisplayP3ColorSpace}
+            />
+          </meshBasicMaterial>
+        </mesh>
+      </Interactive>
+    </group>
   )
 })
